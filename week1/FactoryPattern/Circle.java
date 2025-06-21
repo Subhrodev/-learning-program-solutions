e1e08723476850be69a9afafb77d6621391f131a
Subhrodev <subhrodebb@gmail.com>
2025-06-22 02:32:21 +0530

    Saved changes before deleting week1

--- week1/FactoryPattern/Circle.java
+++ week1/FactoryPattern/Circle.java
@@ -3,7 +3,7 @@ package FactoryPattern;
 public class Circle implements Shape {
     @Override
     public void draw() {
-        System.out.println("Drawing a Circle.");
+        System.out.println("Drawing a Circle");
     }
 }
 
--- week1/FactoryPattern/Rectangle.java
+++ week1/FactoryPattern/Rectangle.java
@@ -4,6 +4,6 @@ package FactoryPattern;
 public class Rectangle implements Shape {
     @Override
     public void draw() {
-        System.out.println("Drawing a Rectangle.");
+        System.out.println("Drawing a Rectangle");
     }
 }
